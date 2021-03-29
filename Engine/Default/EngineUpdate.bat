a3ee78fe6b0e00e470a259074479d78cc6bb4b1d
JeonGeunHee <weiding11086@gmail.com>
2021-03-30 03:29:13 +0900

    ReleaseMode BugFix
    bat file Update, Project Setting Update

--- Engine/Default/EngineUpdate.bat
+++ Engine/Default/EngineUpdate.bat
@@ -1,3 +1,7 @@
-xcopy /y ..\Bin\*.dll ..\..\Client\Bin\
+xcopy /y ..\Bin\*.dll ..\..\Client\Bin\Win32\Debug\
+xcopy /y ..\Bin\*.dll ..\..\Client\Bin\Win32\Release\
+xcopy /y ..\Bin\*.dll ..\..\Client\Bin\x64\Debug\
+xcopy /y ..\Bin\*.dll ..\..\Client\Bin\x64\Release\
+
 xcopy /y ..\Bin\*.lib ..\..\Reference\Lib\
 xcopy /y ..\Header\*.h ..\..\Reference\Header\
