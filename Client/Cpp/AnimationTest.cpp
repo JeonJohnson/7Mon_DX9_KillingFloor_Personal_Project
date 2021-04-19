ce2e2a9a8e7f1388d5e9a082cc0d942765f90027
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2021-04-19 16:39:24 +0900

    210419 1640

---
+++ Client/Cpp/AnimationTest.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+#include "..\Header\AnimationTest.h"
+
+
+AnimationTest::AnimationTest()
+{
+}
+
+
+AnimationTest::~AnimationTest()
+{
+}
+
+void AnimationTest::Initialize()
+{
+}
+
+void AnimationTest::Update()
+{
+	if (MouseDown(KEY_STATE_LMouse))
+	{
+		
+
+	}
+}
+
+void AnimationTest::LateUpdate()
+{
+}
+
+void AnimationTest::ReadyRender()
+{
+}
+
+void AnimationTest::Release()
+{
+}
